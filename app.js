2828a169229128a4ed6a700256d03a8399f3cb60
usmc1371nerd <james.p.dumas@gmail.com>
2023-01-13 17:13:28 -0600

    if and else statements (finished)

--- app.js
+++ app.js
@@ -4,13 +4,20 @@ const buttons = document.querySelectorAll('button')
 
 
 let calculation = []
+let accumulativeCalculation
 
 function calculate(button){
     const value = button.textContent
-
-    calculation.push(value)
-
-    console.log(button)
+    if (value === "CLEAR") {
+        calculation = []
+        screenDisplay.textContent = '.'
+    }else if (value === "=") {
+        screenDisplay.textContent = eval(accumulativeCalculation)
+    } else {
+        calculation.push(value)
+        accumulativeCalculation = calculation.join('')
+        screenDisplay.textContent = accumulativeCalculation
+    }
 
 } 
 

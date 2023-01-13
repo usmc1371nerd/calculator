59ed891c1c9d08f3dffa6d6facdcbd4a9670802a
usmc1371nerd <james.p.dumas@gmail.com>
2023-01-13 16:48:18 -0600

    fixed and error check arrow function eventlist

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-const buttons = document.querySelectorAll('button')
 const screenDisplay = document.querySelector('.screen')
+const buttons = document.querySelectorAll('button')
+
+
+
+let calculation = []
 
 function calculate(button){
+    const value = button.textContent
+
+    calculation.push(value)
+
+    console.log(button)
 
 } 
 
-buttons.forEach(button => button =>addEventListener('click',()=> calculate(button)))
+buttons.forEach(button => button.addEventListener('click', () => calculate(button)))
+
